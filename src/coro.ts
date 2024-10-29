ed1ecb355f91613f9d0d892f3b68bd91608e394b
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-10-29 00:38:49 -0300

    feat!: add actor return method

--- src/coro.ts
+++ src/coro.ts
@@ -3,24 +3,28 @@ import { TIMEOUT } from "./constants/core";
 import { RECEIVE_MESSAGE_TOPIC, SEND_MESSAGE_TOPIC } from "./constants/topics";
 import { BindToCustomMessage } from "./utils/bind-to-custom-message";
 
+type ProcessorActor = Actor & { Processor: BaseScript };
+
 export class Coro {
 	private actorPool = new Array<Actor>();
 	private actorPoolId = 0;
 
 	constructor(
-		private actorTemplate: Actor,
+		private actorTemplate: ProcessorActor,
 		private module: ModuleScript,
 	) {}
 
-	Spawn(...args: unknown[]) {
+	public Spawn(...args: unknown[]) {
 		const actor = this.nextActor();
 		this.actorPoolId += 1;
 
-		actor.SetAttribute("id", this.actorPoolId);
+		const id = this.actorPoolId;
+
+		actor.SetAttribute("id", id);
 		actor.Processor.Enabled = true;
 
 		const promise = new Promise((resolve, reject) => {
-			const connection = BindToCustomMessage(RECEIVE_MESSAGE_TOPIC + this.actorPoolId, (result: unknown) => {
+			const connection = BindToCustomMessage(RECEIVE_MESSAGE_TOPIC + id, (result: unknown) => {
 				connection.Disconnect();
 				resolve(result);
 			});
@@ -31,15 +35,28 @@ export class Coro {
 			});
 		});
 
-		task.defer(() => actor.SendMessage(SEND_MESSAGE_TOPIC + this.actorPoolId, this.module, ...args));
+		task.defer(() => actor.SendMessage(SEND_MESSAGE_TOPIC + id, this.module, ...args));
 
-		return promise;
+		return [promise, () => this.Return(actor)];
+	}
+
+	public Return(actor: ProcessorActor) {
+		actor.Processor.Enabled = false;
+		actor.SetAttribute("id", undefined);
+		this.actorPool.push(actor);
+	}
+
+	public Destroy() {
+		for (const actor of this.actorPool) {
+			actor.Destroy();
+		}
+		this.actorPool.clear();
 	}
 
 	private nextActor() {
 		return (this.actorPool.unorderedRemove(0) ??
 			Clone(this.actorTemplate, {
 				Parent: this.actorTemplate.Parent,
-			})) as Actor & { Processor: BaseScript };
+			})) as ProcessorActor;
 	}
 }
