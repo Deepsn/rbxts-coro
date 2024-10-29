82b72f6b0ae8e28d47d2b17c788001f3a42e18ae
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-10-29 01:24:16 -0300

    fix: client processor parent not running scripts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Clone, Make } from "@rbxts/altmake";
-import { ServerScriptService, StarterPlayer } from "@rbxts/services";
+import { Players, ServerScriptService, StarterPlayer } from "@rbxts/services";
 import { IS_SERVER } from "./constants/core";
 import { Coro } from "./coro";
 import { type Tree, findByTree } from "./utils/find-by-tree";
 
-const processorParent = IS_SERVER ? ServerScriptService : StarterPlayer.WaitForChild("StarterPlayerScripts");
+const processorParent = IS_SERVER ? ServerScriptService : Players.LocalPlayer.WaitForChild("PlayerScripts");
 const processors = script.WaitForChild("processors") as Folder;
 const processorTemplate = processors.WaitForChild(`processor-${IS_SERVER ? "server" : "client"}`) as BaseScript;
 
