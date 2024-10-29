c926bc768250f75064e0f7e47b8860c6541e8c35
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-10-28 23:41:55 -0300

    feat: add client processor

--- src/processors/processor-client.client.ts
+++ src/processors/processor-client.client.ts
@@ -1,1 +1,19 @@
-print("running");
+import { RECEIVE_MESSAGE_TOPIC, SEND_MESSAGE_TOPIC } from "../constants/topics";
+import { SendCustomMessage } from "../utils/bind-to-custom-message";
+
+const actor = script.GetActor();
+assert(actor, "Processor must be a child of an actor");
+
+const id = actor.GetAttribute("id");
+
+actor.BindToMessage(SEND_MESSAGE_TOPIC + id, (module: ModuleScript, ...args: unknown[]) => {
+	const execute = require(module) as (...args: unknown[]) => unknown;
+
+	task.desynchronize();
+
+	const result = execute(...args);
+
+	task.synchronize();
+
+	SendCustomMessage(RECEIVE_MESSAGE_TOPIC + id, result);
+});
