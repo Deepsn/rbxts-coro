933287f664df85913259482e58492c8ac5a0641f
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-10-29 00:21:30 -0300

    fix: only verify and create messenger event instead of waiting on the client

--- src/utils/bind-to-custom-message.ts
+++ src/utils/bind-to-custom-message.ts
@@ -2,15 +2,12 @@ import { Make } from "@rbxts/altmake";
 import { ReplicatedStorage } from "@rbxts/services";
 import { IS_SERVER } from "../constants/core";
 
-const messenger = (
-	IS_SERVER
-		? (ReplicatedStorage.FindFirstChild("CoroMessenger") ??
-			Make("BindableEvent", {
-				Name: "CoroMessenger",
-				Parent: ReplicatedStorage,
-			}))
-		: ReplicatedStorage.WaitForChild("CoroMessenger")
-) as BindableEvent;
+const messengerKey = `CoroMessenger-${IS_SERVER ? "Server" : "Client"}`;
+const messenger = (ReplicatedStorage.FindFirstChild(messengerKey) ??
+	Make("BindableEvent", {
+		Name: messengerKey,
+		Parent: ReplicatedStorage,
+	})) as BindableEvent;
 
 export function BindToCustomMessage(topic: string, callback: (...args: unknown[]) => void) {
 	return messenger.Event.Connect((topicReceived: string, ...args: unknown[]) => {
